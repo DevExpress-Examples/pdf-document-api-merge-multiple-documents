a91b6bb73081f878d838fac8ae55d53bfb5906ff
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/PdfMergeExample/Program.cs
@@ -0,0 +1,15 @@
+﻿using DevExpress.Pdf;
+
+namespace PdfMergeExample {
+
+    class Program {
+        static void Main(string[] args) {
+
+            using (PdfDocumentProcessor pdfDocumentProcessor = new PdfDocumentProcessor()) {
+                pdfDocumentProcessor.CreateEmptyDocument("..\\..\\docs\\Merged.pdf");
+                pdfDocumentProcessor.AppendDocument("..\\..\\docs\\TextMerge1.pdf");
+                pdfDocumentProcessor.AppendDocument("..\\..\\docs\\TextMerge2.pdf");             
+            }
+        }
+    }
+}
---
+++ VB/PdfMergeExample/Program.vb
@@ -0,0 +1,15 @@
+﻿Imports DevExpress.Pdf
+
+Namespace PdfMergeExample
+
+    Friend Class Program
+        Shared Sub Main(ByVal args() As String)
+
+            Using pdfDocumentProcessor As New PdfDocumentProcessor()
+                pdfDocumentProcessor.CreateEmptyDocument("..\..\docs\Merged.pdf")
+                pdfDocumentProcessor.AppendDocument("..\..\docs\TextMerge1.pdf")
+                pdfDocumentProcessor.AppendDocument("..\..\docs\TextMerge2.pdf")
+            End Using
+        End Sub
+    End Class
+End Namespace
