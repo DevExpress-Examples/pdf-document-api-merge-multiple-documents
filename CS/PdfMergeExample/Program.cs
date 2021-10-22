94aa31c04cabab8ab8d7285565c9b5569835c558
geumjaLee <polina.fedorova@devexpress.com>
2021-10-22 11:15:23 +0300

    add savedocument call

--- CS/PdfMergeExample/Program.cs
+++ CS/PdfMergeExample/Program.cs
@@ -1,15 +1,22 @@
 ﻿using DevExpress.Pdf;
 
-namespace PdfMergeExample {
+namespace PdfMergeExample
+{
 
-    class Program {
-        static void Main(string[] args) {
+    class Program
+    {
+        static void Main(string[] args)
+        {
 
-            using (PdfDocumentProcessor pdfDocumentProcessor = new PdfDocumentProcessor()) {
+            using (PdfDocumentProcessor pdfDocumentProcessor = new PdfDocumentProcessor())
+            {
                 pdfDocumentProcessor.CreateEmptyDocument("..\\..\\docs\\Merged.pdf");
                 pdfDocumentProcessor.AppendDocument("..\\..\\docs\\TextMerge1.pdf");
-                pdfDocumentProcessor.AppendDocument("..\\..\\docs\\TextMerge2.pdf");             
+                pdfDocumentProcessor.AppendDocument("..\\..\\docs\\TextMerge2.pdf");
+                pdfDocumentProcessor.SaveDocument("Merged.pdf");
             }
+            System.Diagnostics.Process.Start("Merged.pdf");
         }
+
     }
 }
