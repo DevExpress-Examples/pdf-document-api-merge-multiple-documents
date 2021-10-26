0c46fc27b2d5af670ab5e4e6fdb7b6ea24fd44a2
Poline Fedorova <polina.fedorova@devexpress.com>
2021-10-26 13:35:35 +0300

    Update Program.vb

--- VB/PdfMergeExample/Program.vb
+++ VB/PdfMergeExample/Program.vb
@@ -5,11 +5,13 @@ Namespace PdfMergeExample
     Friend Class Program
         Shared Sub Main(ByVal args() As String)
 
-            Using pdfDocumentProcessor As New PdfDocumentProcessor()
-                pdfDocumentProcessor.CreateEmptyDocument("..\..\docs\Merged.pdf")
-                pdfDocumentProcessor.AppendDocument("..\..\docs\TextMerge1.pdf")
-                pdfDocumentProcessor.AppendDocument("..\..\docs\TextMerge2.pdf")
-            End Using
+Using pdfDocumentProcessor As New PdfDocumentProcessor()
+				pdfDocumentProcessor.CreateEmptyDocument("..\..\docs\Merged.pdf")
+				pdfDocumentProcessor.AppendDocument("..\..\docs\TextMerge1.pdf")
+				pdfDocumentProcessor.AppendDocument("..\..\docs\TextMerge2.pdf")
+				pdfDocumentProcessor.SaveDocument("Merged.pdf")
+End Using
+			System.Diagnostics.Process.Start("Merged.pdf")		
         End Sub
     End Class
 End Namespace
