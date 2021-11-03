dd7d83b9a4acfc2f775a4b7dfc59b4c186cc7f2b
YuliaDX <yuliya.samoylova@devexpress.com>
2021-11-03 10:44:56 +0300

    no message

--- CS/PdfMergeExample/Program.cs
+++ CS/PdfMergeExample/Program.cs
@@ -10,10 +10,10 @@ namespace PdfMergeExample
 
             using (PdfDocumentProcessor pdfDocumentProcessor = new PdfDocumentProcessor())
             {
-                pdfDocumentProcessor.CreateEmptyDocument("..\\..\\docs\\Merged.pdf");
+                pdfDocumentProcessor.CreateEmptyDocument("Merged.pdf");
                 pdfDocumentProcessor.AppendDocument("..\\..\\docs\\TextMerge1.pdf");
                 pdfDocumentProcessor.AppendDocument("..\\..\\docs\\TextMerge2.pdf");
-                pdfDocumentProcessor.SaveDocument("Merged.pdf");
+                
             }
             System.Diagnostics.Process.Start("Merged.pdf");
         }
