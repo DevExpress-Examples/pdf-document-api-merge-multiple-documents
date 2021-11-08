e09a8b7bd8f3696be5a236e0e1a276b1bb97de82
DevExpressExampleBot <fakeEmail>
2021-11-08 23:45:16 +0300

    Source auto update [skip ci]

--- VB/PdfMergeExample/Program.vb
+++ VB/PdfMergeExample/Program.vb
@@ -1,17 +1,17 @@
-﻿Imports DevExpress.Pdf
+Imports DevExpress.Pdf
 
 Namespace PdfMergeExample
 
     Friend Class Program
-        Shared Sub Main(ByVal args() As String)
 
-Using pdfDocumentProcessor As New PdfDocumentProcessor()
-				pdfDocumentProcessor.CreateEmptyDocument("..\..\docs\Merged.pdf")
-				pdfDocumentProcessor.AppendDocument("..\..\docs\TextMerge1.pdf")
-				pdfDocumentProcessor.AppendDocument("..\..\docs\TextMerge2.pdf")
-				pdfDocumentProcessor.SaveDocument("Merged.pdf")
-End Using
-			System.Diagnostics.Process.Start("Merged.pdf")		
+        Shared Sub Main(ByVal args As String())
+            Using pdfDocumentProcessor As PdfDocumentProcessor = New PdfDocumentProcessor()
+                pdfDocumentProcessor.CreateEmptyDocument("Merged.pdf")
+                pdfDocumentProcessor.AppendDocument("..\..\docs\TextMerge1.pdf")
+                pdfDocumentProcessor.AppendDocument("..\..\docs\TextMerge2.pdf")
+            End Using
+
+            System.Diagnostics.Process.Start("Merged.pdf")
         End Sub
     End Class
 End Namespace
